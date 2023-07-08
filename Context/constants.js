feb279a2e308157c40f73548001f3d6ec4f05455
Bharatjain14 <bharatashokjain48@gmail.com>
2023-07-08 06:10:13 +0530

    third

---
+++ Context/constants.js
@@ -0,0 +1,6 @@
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+import chatApp from "./ChatApp.json";
+
+export const ChatAppAddress = "0x9c6fE82728e82BBa092dfEA225D2fc34c939dA5d";
+
+export const ChatAppABI = chatApp.abi;
